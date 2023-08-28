13021341583185e373a3e3f79889dce3eecae224
Jannik_Wiest <jannik.wiest@gmail.com>
2023-08-28 09:32:36 +0200

    Final

--- NumberSort.py
+++ NumberSort.py
@@ -1,12 +1,19 @@
 import random
 
+def BubbleSortNumbers(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+
 data = random.sample(range(0, 100), 100) #No number will be generated twice
 #Beginner method:
 '''for i in range(0,100):
     n = random.randint(1,100)
     data.append(n)'''
 
-print(data)
+print(f'Unsorted: {data}')
 print('---------------------')
-def SortNumbers():
-    pass
+BubbleSortNumbers(data)
+print(f'Sorted array: {data}')
