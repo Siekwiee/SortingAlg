31bc662ee66e92de69294c137f06a8ac11a4286b
Jannik_Wiest <jannik.wiest@gmail.com>
2023-08-28 08:56:57 +0200

    Init

---
+++ NumberSort.py
@@ -0,0 +1,12 @@
+import random
+
+data = random.sample(range(0, 100), 100) #No number will be generated twice
+#Beginner method:
+'''for i in range(0,100):
+    n = random.randint(1,100)
+    data.append(n)'''
+
+print(data)
+print('---------------------')
+def SortNumbers():
+    pass
